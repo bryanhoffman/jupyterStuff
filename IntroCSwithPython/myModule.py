754615b90a766ee1a34ac218dad6d7e801099c8c
MyNameIsNametag <westpalmintern@gmail.com>
2017-11-04 15:15:07 -0500

    Got up to Chapter 4

---
+++ IntroCSwithPython/myModule.py
@@ -0,0 +1,6 @@
+def funky(x):
+    print("Let's get funky "+str(x)+" times!")
+    for i in range(1,x):
+        print("Doh doh doh, doh doh doh, doh doh")
+    print("Doh doh doh, doh duh (Aaaaaaow!)")
+
